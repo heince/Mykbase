d9655680f32de3895fa72d9aa2a4b2b47a3ff49a
Heince Kurniawan <heince@gmail.com>
2012-05-20 14:02:09 +0800

    add function to check tag, category, description

--- Mykbase/Command/Search.pm
+++ Mykbase/Command/Search.pm
@@ -18,8 +18,12 @@ EOF
 sub validate{
     my ($self, $cmd_opts, @args) = @_;
 
-    die $self->usage_text()  . "\n" unless %$cmd_opts;
+	 if(@args){
+		$$cmd_opts{'title'} = $args[0];
+		$$cmd_opts{'description'} = $args[0];
 	 }else{
+		die $self->usage_text()  . "\n" unless %$cmd_opts;
+	 }
 }
 
 sub option_spec {
@@ -31,6 +35,30 @@ sub option_spec {
 	 [ 'description=s'	=> 'Description of kbase' ],
     [ 'exact' => 'exact match' ],
 	 [ 'sort' => 'sort descending'],
+}
+
+#check tag options
+sub check_tag{
+	my ($tag, $exact, $dbh, $obj) = @_;
+	
+	#set the attributes
+	$obj->tag($tag);
+	$obj->exact($exact);
+	$obj->dbh($dbh);
+	
+	$obj->check_tag();   
+}
+
+#check description options
+sub check_description{
+	my ($description, $exact, $dbh, $obj) = @_;
+	
+	#set the attributes
+	$obj->description($description);
+	$obj->exact($exact);
+	$obj->dbh($dbh);
+	
+	$obj->check_description();   
 }
 
 #check title options
@@ -45,20 +73,44 @@ sub check_title{
 	$obj->check_title();   
 }
 
+#check category option
+sub check_category{
+	my ($category, $exact, $dbh, $obj) = @_;
+	
+	#set the attributes
+	$obj->category($category);
+	$obj->exact($exact);
+	$obj->dbh($dbh);
+	
+	$obj->check_category(); 
+}
+
 #check all supported options
 sub check_opts{
     my ($opts, $dbh) = @_;
 	 
 	 #attribute for object
 	 my $obj;
+	 $obj = Search->new();
     
 	 #store exact opts
 	 my $exact = $opts->{'exact'};
 	 
     #check title
 	 my $title = $opts->{'title'};
-	 $obj = Search->new();
-	 check_title($title, $exact, $dbh, $obj);    
+	 check_title($title, $exact, $dbh, $obj) if $title;
+	 
+	 #check category
+	 my $category = $opts->{'cat'};
+	 check_category($category, $exact, $dbh, $obj) if $category;
+	 
+	 #check description
+	 my $description = $opts->{'description'};
+	 check_description($description, $exact, $dbh, $obj) if $description;
+	 
+	 #check tag
+	 my $tag = $opts->{'tag'};
+	 check_tag($tag, $exact, $dbh, $obj) if $tag;
         
 }
 
