4f51028d9535bf1696089515b423ba5e4648c6d1
Heince Kurniawan <heince@gmail.com>
2012-05-20 14:05:31 +0800

    add header when printing, add function to check tag, desc, category

--- Mykbase/lib/Search.pm
+++ Mykbase/lib/Search.pm
@@ -32,46 +32,65 @@ sub get_path_byid(){
 sub print(){
     my $self = shift;
     
+    #get opt and print header
+    my $header = shift;
+    say "== Search by $header ==\n";
+    
     #check result set , print id + title 
     if($self->rs != 0){
         while(my $kbase = $self->rs->next){
             my $dt = $self->convert_sqlite_date($kbase->created);
             say $kbase->id . " || " . $kbase->title . " || " . $kbase->description. " || " . $kbase->category . " || " . $dt;
         }
+        say "";
     }else{
-        say "no record found";
+        say "no record found\n";
     }
 }
 
-#check param , search by title and print the result
-sub check_title(){
+#global check function
+sub check(){
     my $self = shift;
     
-    if($self->title){
+    my $opt = shift;
+    
+    if($self->$opt){
         my $rs;
         if($self->exact){
-            $rs = ${$self->dbh}->resultset('Kbase')->search({ title => $self->title });
+            $rs = ${$self->dbh}->resultset('Kbase')->search({ $opt => $self->$opt });
         }else{
-            $rs = ${$self->dbh}->resultset('Kbase')->search({ title => { -like => "%" . $self->title . "%" } });
+            $rs = ${$self->dbh}->resultset('Kbase')->search({ $opt => { -like => "%" . $self->$opt . "%" } });
         }
         $self->rs($rs);
-		  $self->print();
+		  $self->print($opt);
 	}
 }
 
-#check param , search by title and print the result
+#pass tag to check function
+sub check_tag(){
+    my $self = shift;
+    
+    $self->check('tag');
+}
+
+#pass description to check function
+sub check_description(){
+    my $self = shift;
+    
+    $self->check('description');
+}
+
+#pass title to check function
+sub check_title(){
+    my $self = shift;
+    
+    $self->check('title');
+}
+
+#pass category to check function
 sub check_category(){
     my $self = shift;
     
-    if($self->cat){
-        my $rs;
-        if($self->exact){
-            $rs = ${$self->dbh}->resultset('Kbase')->search({ title => $self->title });
-        }else{
-            $rs = ${$self->dbh}->resultset('Kbase')->search({ title => { -like => "%" . $self->title . "%" } });
-        }
-        $self->rs($rs);
-		  $self->print();
-	}
+    $self->check('category');
 }
 1;
